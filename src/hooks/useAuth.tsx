57f7ac21d4162270d6c28ab0442bbaa861486fc0
Gabriel Souza <gabrielss_@outlook.com>
2021-12-02 11:42:05 -0300

    signOut concluido

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -119,13 +119,32 @@ function AuthProvider({ children }: AuthProviderData) {
   async function signOut() {
     try {
       // set isLoggingOut to true
+      setIsLoggingOut(true);
+
       // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      revokeAsync(
+        {
+          token: userToken,
+          clientId: CLIENT_ID,
+        },
+        {
+          revocationEndpoint: twitchEndpoints.revocation,
+        }
+      );
     } catch (error) {
+      console.log(error);
     } finally {
       // set user state to an empty User object
+      setUser({} as User);
+
       // set userToken state to an empty string
+      setUserToken("");
+
       // remove "access_token" from request's authorization header
+      delete api.defaults.headers.authorization;
+
       // set isLoggingOut to false
+      setIsLoggingOut(false);
     }
   }
 
